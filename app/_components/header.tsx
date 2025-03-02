dfed85ea675cefeee0dfe3c363f50d89847f451b
Marcell Pramana <marcellpramana@gmail.com>
2025-03-02 20:18:47 +0700

    feat: layouting navbar & header

---
+++ app/_components/header.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+const Gradient = () => {
+  return (
+    <div className="absolute bg-[#FFECA2] rounded-full w-[750px] h-[700px] blur-3xl opacity-25 z-10">
+      a
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="h-screen flex flex-col">
+      {/* This div takes up the available space and centers the content */}
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <div className="relative w-full flex justify-center ">
+          <h1 className="absolute -top-4 text-center text-9xl font-semibold text-neutral-800 z-20 font-(family-name:--font-grenze-gotisch)">
+            Daya Anagata Nusantara
+          </h1>
+          <Image
+            className="mx-auto z-30"
+            src="/garuda.png"
+            alt="@garuda"
+            width={700}
+            height={300}
+          />
+        </div>
+        <Gradient />
+      </div>
+
+      {/* This ensures <h5> stays at the bottom */}
+      <h5 className="font-(family-name:--font-grenze-gotisch) text-amber-800 text-center mb-10 text-3xl">
+        “Untuk Kemakmuran Indonesia”
+      </h5>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/_components/navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+const navLinks = [
+  { section: "Tentang", url: "#tentang" },
+  { section: "Perjalanan", url: "#perjalanan" },
+  { section: "Visi & Misi", url: "#visi&misi" },
+  { section: "Narahubung", url: "#narahubung" },
+];
+
+const Navbar = () => {
+  return (
+    <div className="fixed w-full mt-3 z-[60]">
+      <a href="/">
+        <Image
+          className="absolute left-5"
+          src={"/logoBlack.png"}
+          alt="@icon-black"
+          width={120}
+          height={120}
+        />
+      </a>
+
+      <nav className="mx-auto  w-fit flex gap-3 shadow-inner  rounded-xl font-serif border backdrop-blur-lg  bg-gradient-to-r from-neutral-300/70 to-red-300/60">
+        {navLinks.map(({ section, url }, i) => (
+          <a
+            key={url}
+            href={url}
+            //   still not working
+            className={` hover:bg-amber-50/50 text-sm transition-colors py-2.5 px-3 rounded-xl  ${
+              i === 0 && "pl-1"
+            }, ${i === 2 && "pr-1"}`}
+          >
+            {section}
+          </a>
+        ))}
+        <span className="bg-amber-200 py-2.5 px-3 rounded-xl border-l ">🇮🇩</span>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
